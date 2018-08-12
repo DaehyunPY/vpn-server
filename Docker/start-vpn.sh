971b405697663f89454afe0a25d15392081ad173
Daehyun You <daehyun.park.you@gmail.com>
2018-08-12 23:07:14 +0900

    Update run.sh and Docker/*

--- Docker/start-vpn.sh
+++ Docker/start-vpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# ufw disable
 # ufw enable
-strongswan restart --nofork
+strongswan start --nofork
+# ufw disable
--- run.sh
+++ run.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 dir=$(dirname $(realpath $0))
-
-# docker run --name vpn-server \
-#     --cap-add=NET_ADMIN \
-#     -d --restart=unless-stopped \
-#     -v $dir/ufw:/etc/ufw \
-#     -v $dir/strongswan:/etc/strongswan \
-#     -p 500:500 \
-#     -p 4500:4500 \
-#     daehyunpy/vpn-server
-
-docker run -it --rm --cap-add=NET_ADMIN \
-    -v /usr/local/etc/ipsec.conf:/etc/strongswan/ipsec.conf \
-    -v /usr/local/etc/ipsec.secrets:/etc/strongswan/ipsec.secrets \
-    -v /usr/local/etc/ipsec.d:/etc/strongswan/ipsec.d \
+docker run --name vpn-server \
+    --cap-add=NET_ADMIN \
+    -d --restart=unless-stopped \
+    -v $dir/strongswan:/etc/strongswan \
+    -v $dir/ufw:/etc/ufw \
     -p 500:500/udp \
     -p 4500:4500/udp \
-    vpn-server:dev bash
+    daehyunpy/vpn-server
