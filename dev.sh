91b9669d2378bb555fab790d1061f0ebeeac73df
Daehyun You <daehyun.park.you@gmail.com>
2018-08-13 15:13:59 +0900

    Fix dev.sh and run.sh for Fedora

--- dev.sh
+++ dev.sh
@@ -3,8 +3,8 @@ dir=$(dirname $(realpath $0))
 docker run \
     --cap-add=NET_ADMIN \
     -it --rm \
-    -v $dir/strongswan:/etc/strongswan \
-    -v $dir/ufw:/etc/ufw \
+    -v $dir/strongswan:/etc/strongswan:Z \
+    -v $dir/ufw:/etc/ufw:Z \
     -p 500:500/udp \
     -p 4500:4500/udp \
     $* \
--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ dir=$(dirname $(realpath $0))
 docker run --name vpn-server \
     --cap-add=NET_ADMIN \
     -d --restart=unless-stopped \
-    -v $dir/strongswan:/etc/strongswan \
-    -v $dir/ufw:/etc/ufw \
+    -v $dir/strongswan:/etc/strongswan:Z \
+    -v $dir/ufw:/etc/ufw:Z \
     -p 500:500/udp \
     -p 4500:4500/udp \
     $* \
