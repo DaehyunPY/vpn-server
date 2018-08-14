0e08c44741b2555875e80747bbaa138b9376db18
Daehyun You <daehyun.park.you@gmail.com>
2018-08-14 20:24:39 +0900

    Update start-vpn.sh files

--- Docker/start-vpn.sh
+++ Docker/start-vpn.sh
@@ -9,9 +9,7 @@ sleep 1
 if ps -p $pid >&-
 then
     sleep 4
-    swanctl --load-creds
-    swanctl --load-pools
-    swanctl --load-conns
+    swanctl --load-all
     wait $pid
 fi
 
--- macOS/start-vpn.sh
+++ macOS/start-vpn.sh
@@ -6,8 +6,6 @@ sleep 1
 if ps -p $pid >&-
 then
     sleep 4
-    swanctl --load-creds
-    swanctl --load-pools
-    swanctl --load-conns
+    swanctl --load-all
     wait $pid
 fi
